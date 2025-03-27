a20d8bd3bb0492a9fc54a159f53dc08c7866d5de
cheetodustflori <florianneche@gmail.com>
2025-03-27 13:38:03 -0500

    updated inex and main w bootstrap

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/pages/App.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import './index.css'; // we'll add Tailwind here later
 
