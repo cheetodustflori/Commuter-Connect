818264470332b18579d67ddb32caa6eab5b8b0a9
zpate6 <zpate6@uic.edu>
2025-03-28 18:29:42 -0500

    changes to commute schedule component: added commute buddies

--- frontend/src/Components/Schedule/CommuteRoute.jsx
+++ frontend/src/Components/Schedule/CommuteRoute.jsx
@@ -4,7 +4,7 @@ import trainLogo from '../../Images/trainLogo.png'
 import { useState, useEffect } from "react";
 
 
-const CommuteRoute = ({ totalTime, isActive }) => {
+const CommuteRoute = ({ isActive, totalTime, overallTime, routeTitle, routeStatus, startLocation, endLocation, departTime, arrivalTime, buddies }) => {
 
     const [timeLeft, setTimeLeft] = useState(totalTime);
 
@@ -21,50 +21,71 @@ const CommuteRoute = ({ totalTime, isActive }) => {
     const percentagePassed = ((totalTime - timeLeft) / totalTime) * 100;
 
   return (
-    <div className='routes'>
-        <h3 style={{marginTop: isActive ? '-30px' : '-80px',}} id='mainTime'>8 AM</h3>
+    <div className='commuteRouteComp'>
 
-        <div style={{color: isActive ? 'rgba(0, 0, 0, 0.302)' : 'black',}} className='route'>
+        <div className='routes'>
 
-            <div className='nameAndStatus'>
-                <p>Home to School</p>
-                <p> | </p>
-                <p style={{color: isActive ? 'rgba(0, 0, 0, 0.302)' : '#48C738',}}>En Route</p>
-                <img style={{opacity: isActive ? '0.5' : '1',}}  id="trainImage" src={trainLogo}/>
-            </div>
+            <h3 style={{marginTop: !isActive ? '-30px' : '-80px',}} id='mainTime'>{overallTime}</h3>
 
-            <div className='toAndFromText'>
-                <p>1200 W Harrison St, Chicago, IL 60607</p>
-                <p style={{fontWeight: 'bold'}} >600 E Grand Ave, Chicago, IL 60611</p>
-            </div>
+            <div style={{color: !isActive ? 'rgba(0, 0, 0, 0.302)' : 'black',}} className='route'>
 
-            <div className='progressBarAndInfo'>
-                <div className="progressBar">
-                    {!isActive && (
+                <div className='nameAndStatus'>
+                    <p>{routeTitle}</p>
+                    <p> | </p>
+                    <p style={{color: !isActive ? 'rgba(0, 0, 0, 0.302)' : '#48C738',}}>{routeStatus}</p>
+                    <img style={{opacity: !isActive ? '0.5' : '1',}}  id="trainImage" src={trainLogo}/>
+                </div>
+
+                <div className='toAndFromText'>
+                    <p>{startLocation}</p>
+                    <p style={{fontWeight: 'bold'}} >{endLocation}</p>
+                </div>
+
+                <div className='progressBarAndInfo'>
+                    <div className="progressBar">
+                        {isActive && (
+                            <>
+                                <div id="circle" style={{ left: `${percentagePassed}%` }}></div>
+                                <div id="progress" style={{ width: `${percentagePassed}%` }}></div>
+                            </>
+                        )}
+                    </div>
+                    
+                    {isActive && (
                         <>
-                            <div id="circle" style={{ left: `${percentagePassed}%` }}></div>
-                            <div id="progress" style={{ width: `${percentagePassed}%` }}></div>
+                        <div className='estDepartureAndArrival'>
+                            <div id='infoText'>
+                                <p style={{fontWeight: 'bold'}}>Depart</p>
+                                <p>{departTime}</p>
+                            </div>
+
+                            <div id='infoText'>
+                                <p style={{fontWeight: 'bold'}}>Est. Arrival</p>
+                                <p>{arrivalTime}</p>
+                            </div>
+                        </div>
                         </>
                     )}
                 </div>
-                
-                {!isActive && (
-                    <>
-                    <div className='estDepartureAndArrival'>
-                        <div id='infoText'>
-                            <p style={{fontWeight: 'bold'}}>Depart</p>
-                            <p>8:00 AM</p>
-                        </div>
-
-                        <div id='infoText'>
-                            <p style={{fontWeight: 'bold'}}>Est. Arrival</p>
-                            <p>8:15 AM</p>
-                        </div>
-                    </div>
-                    </>
-                )}
             </div>
-            
+        </div>
+
+        <div className='commuteBuddies'>
+
+            {buddies.map((person) => (
+                <div id='user' key={person.id}>
+                
+                    <div style={{
+                        width: "10px",
+                        height: "10px",
+                        backgroundColor: `${person.color}`,
+                        borderRadius: "50%",
+                        }}>
+                    </div>
+                    <p style={{fontWeight: 'bold'}}>{person.name}</p>
+
+                </div>
+            ))}
 
         </div>
 
--- frontend/src/Pages/Schedule.jsx
+++ frontend/src/Pages/Schedule.jsx
@@ -3,22 +3,58 @@ import './Styles/Schedule.css'
 import CommuteRoute from '../Components/Schedule/CommuteRoute'
 
 const Schedule = () => {
+    const usersArray = [
+        { id: 1, name: "Ted", color: "#769EB8" },
+        { id: 2, name: "Robin", color: "#EC7D0E" },
+      ];
+
   return (
-    <body className='scheduleMain'>
-        <div className='scheduleAndBuddiesComponent'>
-            <div className='todaysCommuteScedule'>
+    <div className='scheduleAndBuddiesComponent'>
+
+        <div className='todaysCommuteScedule'>
+
+            <div className='fullTitle'>
                 <div id='titleBar'>
                     <h2>Today’s Commute Schedule</h2>
                     <p id="editScheduleId" style={{ cursor: 'pointer', textDecoration: "underline" }} >Edit Schedule</p>
                 </div>
-                <div>
-                    <CommuteRoute totalTime={100}/>
-                    <CommuteRoute totalTime={100} isActive={true}/>
-                </div>
-
+                <h2>Commute Buddies</h2>
             </div>
+                
+            <div>
+                {/* Loop through the commute routes using a map here (look at CommuteRoute.jsx under the commuteBuddies div for an example) */}
+                
+                <CommuteRoute 
+                    isActive={true}
+                    totalTime={100}
+                    overallTime={"8 AM"}
+                    routeTitle={"Home to School"}
+                    routeStatus={"En Route"}
+                    startLocation={"1200 W Harrison St, Chicago, IL 60607"}
+                    endLocation={"600 E Grand Ave, Chicago, IL 60611"}
+                    departTime={"8:00 AM"}
+                    arrivalTime={"8:15 AM"}
+                    buddies={usersArray}
+                />
+                <CommuteRoute 
+                    isActive={false}
+                    totalTime={100} 
+                    overallTime={"10 AM"}
+                    routeTitle={"School to Home"}
+                    routeStatus={"Upcoming"}
+                    startLocation={"1200 W Harrison St, Chicago, IL 60607"}
+                    endLocation={"600 E Grand Ave, Chicago, IL 60611"}
+                    departTime={"8:00 AM"}
+                    arrivalTime={"8:15 AM"}
+                    buddies={[{ id: 1, name: "Lily", color: "#48C738" }]}
+                />
+            </div>
+
         </div>
-    </body>
+
+        <div id='line'/>
+
+    </div>
   )
 }
 
