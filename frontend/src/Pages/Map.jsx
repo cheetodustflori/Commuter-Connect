5642fc74df699b9d150da73f6139d3bcc3cd998a
cheetodustflori <florianneche@gmail.com>
2025-04-21 16:31:29 -0500

    protected route

--- frontend/src/Pages/Map.jsx
+++ frontend/src/Pages/Map.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import NavBar from "../components/NavBar/Nav";
+import { useState } from "react";
 
 export default function Map() {
+  const [activeStations, setActiveStations] = useState([]);
+  const [activeFood, setActiveFood] = useState([]);
+  const [activeStudy, setActiveStudy] = useState([]);
+
+  const toggleItem = (item, list, setList) => {
+    if (list.includes(item)) {
+      setList(list.filter((i) => i !== item)); // remove it
+    } else {
+      setList([...list, item]); // add it
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -15,28 +28,84 @@ export default function Map() {
         </div>
         <div id="vl"></div>
         <div id="map-options">
-            <div id="stations" className="grid-item">
-                <h3>Stations</h3>
-                <ul>
-                    <li id="bus-stations">Bus Stations</li>
-                    <li id="train-stations">Train Stations</li>
-                </ul>
-            </div>
-            <div id="food" className="grid-item">
+          <div id="stations" className="grid-item">
+            <h3>Stations</h3>
+            <ul>
+              <li
+                id="bus-stations"
+                className={activeStations.includes("bus") ? "active" : ""}
+                onClick={() =>
+                  toggleItem("bus", activeStations, setActiveStations)
+                }
+              >
+                Bus Stations
+              </li>
+
+              <li
+                id="train-stations"
+                className={activeStations.includes("train") ? "active" : ""}
+                onClick={() =>
+                  toggleItem("train", activeStations, setActiveStations)
+                }
+              >
+                Train Stations
+              </li>
+            </ul>
+          </div>
+
+          <div id="food" className="grid-item">
             <h3>Food</h3>
-                <ul>
-                    <li id="fast-food">Fast Food</li>
-                    <li id="cafe">Cafe</li>
-                    <li id="groceries">Groceries</li>
-                </ul>
-            </div>
-            <div id="study" className="grid-item">
+            <ul>
+              <li
+                id="fast-food"
+                className={activeFood.includes("fast") ? "active" : ""}
+                onClick={() => toggleItem("fast", activeFood, setActiveFood)}
+              >
+                Fast Food
+              </li>
+
+              <li
+                id="cafe"
+                className={activeFood.includes("cafe") ? "active" : ""}
+                onClick={() => toggleItem("cafe", activeFood, setActiveFood)}
+              >
+                Cafe
+              </li>
+              <li
+                id="groceries"
+                className={activeFood.includes("groceries") ? "active" : ""}
+                onClick={() =>
+                  toggleItem("groceries", activeFood, setActiveFood)
+                }
+              >
+                Groceries
+              </li>
+            </ul>
+          </div>
+
+          <div id="study" className="grid-item">
             <h3>Study Spots</h3>
-                <ul>
-                    <li id="library">Library</li>
-                    <li id="campus-space">Campus Space</li>
-                </ul>
-            </div>
+            <ul>
+              <li
+                id="library"
+                className={activeStudy.includes("library") ? "active" : ""}
+                onClick={() =>
+                  toggleItem("library", activeStudy, setActiveStudy)
+                }
+              >
+                Library
+              </li>
+              <li
+                id="campus-space"
+                className={activeStudy.includes("campus-space") ? "active" : ""}
+                onClick={() =>
+                  toggleItem("campus-space", activeStudy, setActiveStudy)
+                }
+              >
+                Campus Space
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
