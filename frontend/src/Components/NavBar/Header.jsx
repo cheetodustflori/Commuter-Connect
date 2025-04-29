ff95baa73d83b22ecc2ae76d4c268247eedd9269
zpate6 <zpate6@uic.edu>
2025-04-29 12:21:02 -0500

    added animation

--- frontend/src/Components/NavBar/Header.jsx
+++ frontend/src/Components/NavBar/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../../../styles.css";
+import { motion, useScroll , useAnimation} from "framer-motion"
 
 export default function Header() {
   var [date, setDate] = useState(new Date());
@@ -35,7 +36,13 @@ export default function Header() {
   return (
     <>
       <div className="header">
-        <img className="train" src="src/assets/train.svg"/>
+      <motion.div className='indJobComp'
+        whileInView={{ x: 0, // Start at the final position (0)
+        opacity: 1,  transition: { duration: 1 }}} initial={{x: '-10vw', opacity: 0}}
+        >
+          <img className="train" src="src/assets/train.svg"/>
+        </motion.div>
+        
         <div className="date-details">
           <p className="day">{day}</p>
           <p className="month-date">{monthDate}</p>
